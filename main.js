d7d58cb0a2c5d5f4ef4c9c5c2526c598f1ab2e25
Hridayy <73392185+Hridayy@users.noreply.github.com>
2022-06-16 20:11:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+function tv(){
+    window.location="tv.html"; 
+}
+function bottle(){
+    window.location="bottle.html"; 
+}
+function desk(){
+    window.location="desk.html"; 
+}
+function fruit(){
+    window.location="fruit.html"; 
+}
---
+++ tv.js
@@ -0,0 +1,43 @@
+var img = ""
+var Status = ""
+objects = [];
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+}
+function preload() {
+    img = loadImage("tv.jpg")
+    Status = "true"
+    objectDetector.detect(img, gotresults)
+}
+function gotresults(error, results) {
+    if (error) {
+        console.log(error)
+    }
+    console.log(results)
+    objects = results;
+
+}
+function back() {
+    window.location = "index.html";
+}
+function modelLoaded() {
+    console.log("modelLoaded")
+}
+function draw() {
+    image(img, 0, 0, 500, 500)
+    if (Status == "true") {
+        for (i = 0; i < objects.length; i++) {
+            fill("red");
+            console.log("fill")
+            text(objects[i].label, objects[i].x + 15, objects[i].y + 15)
+            console.log("text")
+            document.getElementById("Status").innerHTML = "Status = Object Detected"
+            noFill()
+            stroke("red")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+
+}
